96d6a0b1c33d5fb30ff36eb730e28af998618b3d
MuhammadRezaMahmoudi <mrphantom582@gmail.com>
2017-07-06 11:53:06 +0430

    esm man

--- src/MainProgrammer.java
+++ src/MainProgrammer.java
@@ -4,7 +4,7 @@ public class MainProgrammer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Mohsen");
+		System.out.println("muhammadreza");
 		CLI cli= new CLI();
 		cli.CLIi();
 	

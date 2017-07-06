7658f1ce034462ab8bb988d56f3b01a6397b3006
wave2010 <smaghili1371@gmail.com>
2017-07-06 11:31:29 +0430

    Add Coomit in Main

--- src/MainProgrammer.java
+++ src/MainProgrammer.java
@@ -3,12 +3,18 @@ public class MainProgrammer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Salam");
+
+		System.out.println("Salcam");
+
+		//In The Name Of God;
+		System.out.println("Hi");
+
 		CLI cli= new CLI();
 		cli.CLIi();
 		System.out.println("khodaHafez");
 		
 		
+	
 	}
 
 }

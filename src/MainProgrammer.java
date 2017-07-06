3568eaf8b78aa8e7ce846911e9becdd3c608faa5
stalkerjj <mohsen1076@gmail.com>
2017-07-06 11:52:59 +0430

    change3

--- src/MainProgrammer.java
+++ src/MainProgrammer.java
@@ -4,7 +4,7 @@ public class MainProgrammer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!1");
+		System.out.println("Mohsen");
 		CLI cli= new CLI();
 		cli.CLIi();
 	

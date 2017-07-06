eaaa24cc8e65dcf92847279838be14ddfc0b77ed
wave2010 <smaghili1371@gmail.com>
2017-07-06 12:03:12 +0430

    change0

--- src/MainProgrammer.java
+++ src/MainProgrammer.java
@@ -4,7 +4,7 @@ public class MainProgrammer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("muhammadreza");
+		System.out.println("mohsen");
 		CLI cli= new CLI();
 		cli.CLIi();
 	
